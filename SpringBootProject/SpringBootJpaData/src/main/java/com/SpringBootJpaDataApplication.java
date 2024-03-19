28f2e8f0cdd1eb33c685997a03cad5cceb4eafd1
tinonagal <tinonagal@gmail.com>
2024-03-19 17:27:32 +1300

    new files

---
+++ SpringBootProject/SpringBootJpaData/src/main/java/com/SpringBootJpaDataApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaDataApplication.class, args);
+	}
+
+}
